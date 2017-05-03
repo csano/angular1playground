cc970cd97daacb6a7987e0d8486de14ad58de2e0
Chris Sano <csano@microsoft.com>
2017-05-02 17:37:05 -0700

    Work in progress

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -4,24 +4,9 @@
 
 var phonecatApp: ng.IModule = angular.module('phonecatApp', [
   'ngRoute',
-  'phonecatAnimations',
-  'phonecatControllers',
-  'phonecatFilters',
   'phonecatServices'
 ]);
 
-phonecatApp.config(['$routeProvider',
-  function($routeProvider: ng.route.IRouteProvider) {
-    $routeProvider.
-      when('/phones', {
-        templateUrl: 'partials/phone-list.html',
-        controller: 'PhoneListCtrl'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/phones'
-      });
-  }]);
+phonecatApp.controller('PhoneListController', ($scope) => {
+
+});
