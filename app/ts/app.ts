4ba519661ebd68bb7f91ab190cc956bcd759818f
Chris Sano <cmsano@gmail.com>
2017-05-03 09:39:15 -0700

    Restore point

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,62 +1,11 @@
-'use strict'; //do we need this?
-
-/* App Module */
-
-/*
-@csano You're not specifying the dependencies when registering Foo.
-You need to do the exact same thing as you're doing with MyController.
-.service('Foo', ($resource, $q) => .service('Foo', ['$resource', '$q', ($resource, $q)
-And you shouldn't make use of arrow's for services and controllers as you're unable to use new on an arrow function.
-If you're using TypeScript you should use classes as services and controllers.
-So: service('myService', ServiceClass) and controller('myController', ControllerClass)
-*/
-
-// m.controller('MyController', ['$scope', 'Foo', ($scope, f) => {
-//   console.log('here');
-// }]);
-
-
-var m = angular.module('myApp', ['ngRoute', 'ngResource', 'testModule']);
-
-class FooService {
-  constructor(private $resource: ng.resource.IResourceService, $q: ng.IQService) { 
-    
-  }
-}
+'use strict'; 
 
 class MyController {
   constructor(foo: FooService) {
-    console.log('controller constructor');
+    console.log(foo.doSomething());
   }
 }
 
-angular
-  .module('testModule', [])
-  .service('Foo', ['$resource', '$q', FooService])
-  .controller('MyController', ['Foo', MyController]);
+var m = angular.module('myApp', ['ngRoute', 'ngResource', 'testModule', 'components']);
 
-// class BarService {
-//   constructor(private foo: FooService) {
-//     console.log('bar');
-//   }
-// }
-
-// var m: ng.IModule = angular.module('testServices', []);
-
-// m.service('Foo', ($resource, $q) => {
-//   return new FooService($resource, $q);
-// });
-
-// m.service('Bar', (FooService) => {
-//   return new BarService(FooService);
-// });
-// function GreetUserController() {
-//   this.user = 'world';
-// }
-
-// angular.
-//   module('testServices').
-//   component('helloWorld', {
-//     template: 'Hello, {{$ctrl.user}}!',
-//     controller: ['Foo', GreetUserController]
-//   })
+m.controller('MyController', ['Foo', MyController]);
---
+++ app/ts/helloworld.component.ts
@@ -0,0 +1,15 @@
+
+
+class HelloWorldController {
+  public name : String = 'World!'
+  constructor($scope: ng.IScope) {
+    $scope.name = this.name;
+  }
+}
+
+angular.
+  module('components', []).
+  component('helloWorld', {
+    template: `<h1>Hello, {{name}}</h1>`,
+    controller: HelloWorldController
+  });
