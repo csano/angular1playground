65c80170528be7e792057a12e6194d0537d85937
Chris Sano <cmsano@gmail.com>
2017-05-03 09:03:15 -0700

    Restore point

--- app/ts/hello-world.component.ts
+++
@@ -1,9 +0,0 @@
-
-angular.
-  module('myApp').
-  component('greetUser', {
-    template: 'Hello, {{$ctrl.user}}!',
-    controller: function GreetUserController() {
-      this.user = 'world';
-    }
-  })
--- app/ts/services.ts
+++ app/ts/services.ts
@@ -1,19 +1,2 @@
 'use strict';
 
-/* Services */
-class PhoneService {
-  //static $inject = ['$resource'];
-  constructor(private $resource: ng.resource.IResourceService, $q: ng.IQService) { }
-
-  resource = this.$resource('phones/:phoneId.json', {}, {
-    query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
-  });
-}
-
-var phonecatServices: ng.IModule = angular.module('phonecatServices', ['ngResource']);
-
-var m = angular.module('phonecatServices');
-
-m.service('Phone', ($resource, $q) => {
-  return new PhoneService($resource, $q);
-});
