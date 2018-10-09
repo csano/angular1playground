418060661346ee6490aa7e1cc94defedc6f7cff3
Chris Sano <csano@microsoft.com>
2018-10-09 15:48:20 -0700

    Apollo integration

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,2 +1,21 @@
+/*
+import AngularApollo from 'angular1-apollo';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import gql from 'graphql-tag';
+import angular from 'angular';
+import * as angular from 'angular';
+*/
+
 angular
   .module('myApp', ['ngRoute']);
+  /*
+  .module('myApp', ['ngRoute', AngularApollo])
+  .config(apolloProvider => {
+    apolloProvider.defaultClient(new ApolloClient({
+      link: new HttpLink({ uri: '' }),
+      cache: new InMemoryCache()
+    }));
+  });
+  */
