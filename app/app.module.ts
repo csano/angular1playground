5726fbd84de7152de3a46f657dfdc64e4e88b525
Chris Sano <cmsano@gmail.com>
2018-10-11 13:41:29 -0700

    Things are finally working!

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,21 +1,22 @@
-/*`
 import AngularApollo from 'angular1-apollo';
-import { ApolloClient } from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+/*`
 // import gql from 'graphql-tag';
 // import angular from 'angular';
-import * as angular from 'angular';
 */
+import * as angular from 'angular';
 
 angular
-  .module('myApp', ['ngRoute']);
-  /*
+  // .module('myApp', ['ngRoute']);
   .module('myApp', ['ngRoute', AngularApollo])
   .config(apolloProvider => {
-    apolloProvider.defaultClient(new ApolloClient({
-      link: new HttpLink({ uri: '' }),
-      cache: new InMemoryCache()
-    }));
+    const client = new ApolloClient({
+        networkInterface: createNetworkInterface({
+          uri: 'https://graphql.example.com',
+        }),
+      }
+    );
+    apolloProvider.defaultClient(client);
   });
-  */
