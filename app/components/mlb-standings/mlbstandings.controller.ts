c84468c388a46d57866dad3ca4722ef90d313a4f
Chris Sano <cmsano@gmail.com>
2018-10-24 00:09:00 -0700

    Finally have things working with Apollo service

--- app/components/mlb-standings/mlbstandings.controller.ts
+++ app/components/mlb-standings/mlbstandings.controller.ts
@@ -1,19 +1,28 @@
 import * as angular from 'angular';
 import { StandingsService } from '../../services/standings/standingsService';
+import { LeagueStanding } from '../../models/standings/leagueStanding';
+import { TeamStanding } from '../../models/standings/teamStanding';
 
 class MlbStandingsController {
   public standings;
-  constructor($scope: any, standingsService: StandingsService) {
-    console.log('scope: ' + $scope);
-    standingsService.getStandings().then(result => {
-      $scope.standings = result;
+  private _$q_;
+  private _standingsService_;
+  constructor($scope: any, $q: any, standingsService: StandingsService) {
+    this._$q_ = $q;
+
+    standingsService.getStandings().then((r) => {
+      $scope.standings = r;
     });
   }
+
 }
 
 angular
   .module('myApp')
   .component('mlbStandings', {
+    bindings: {
+      standings: '='
+    },
     templateUrl: './components/mlb-standings/mlb-standings.html',
-    controller: ['$scope', 'StandingsService', MlbStandingsController]
+    controller: ['$scope', '$q', 'StandingsService', MlbStandingsController]
   });
--- app/services/standings/standingsService.ts
+++ app/services/standings/standingsService.ts
@@ -2,16 +2,56 @@ import * as angular from 'angular';
 import { ApolloClient, createNetworkInterface } from 'apollo-client';
 import { LeagueStanding } from '../../models/standings/leagueStanding';
 import { ApolloService } from '../apollo/apolloService';
+import { resultKeyNameFromField } from '../../../node_modules/apollo-client/data/storeUtils';
 
 
 export class StandingsService {
-  constructor($http: any, private apolloService: ApolloService) {
-    console.log(apolloService);
+  private _$q_;
+  constructor($http: any, $q: any, private apolloService: ApolloService) {
+    this._$q_ = $q;
   }
 
-  public getStandings(): Promise<LeagueStanding[]> {
+  public getStandings(): any {
+    const deferred = this._$q_.defer();
+
+    const as = this.apolloService;
+    deferred.resolve(as.query('').then(r => {
+      const standings = r.data['standings'] as LeagueStanding[];
+      const out = [];
+      for (const ls of standings) {
+        const teams = [];
+        for (const team of ls.teams) {
+          teams.push({
+            name: team.name,
+            wins: team.wins,
+            losses: team.losses,
+            winningPercentage: team.winningPercentage
+          });
+        }
+        out.push({
+          name: ls.name,
+          teams: teams
+        });
+      }
+      return out;
+    }));
+     /*
+      deferred.resolve([{
+        name: 'foobar',
+        teams: [{
+          name: 'test',
+          wins: 1,
+          losses: 0,
+          winningPercentage: 1.000
+        }]
+      }]);
+      */
+
+    return deferred.promise;
+ 
+    console.log('getStandings');
     return this.apolloService.query('').then(result => {
-      return result.data as LeagueStanding[];
+      return result.data['standings'] as LeagueStanding[];
     });
     /*
     return [{
@@ -54,4 +94,4 @@ export class StandingsService {
 
 angular
   .module('services')
-  .service('StandingsService', ['$http', 'ApolloService', StandingsService]);
+  .service('StandingsService', ['$http', '$q', 'ApolloService', StandingsService]);
